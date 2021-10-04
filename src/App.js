a4a3e18a07c426a3fa03c3059819cb63c042852e
mayankarote <mayankarote1832000@gmail.com>
2021-10-04 16:56:07 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -2,29 +2,6 @@ import { useFormik } from "formik";
 import "./App.css";
 import * as Yup from "yup";
 
-const validate = (values) => {
-  const errors = {};
-  if (!values.firstName) {
-    errors.firstName = "Required";
-  } else if (values.firstName.length > 15) {
-    errors.firstName = "Must be 15 characters or less";
-  }
-
-  if (!values.lastName) {
-    errors.lastName = "Required";
-  } else if (values.lastName.length > 20) {
-    errors.lastName = "Must be 20 characters or less";
-  }
-
-  if (!values.email) {
-    errors.email = "Required";
-  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-    errors.email = "Invalid email address";
-  }
-
-  return errors;
-};
-
 function App() {
   const formik = useFormik({
     initialValues: {
